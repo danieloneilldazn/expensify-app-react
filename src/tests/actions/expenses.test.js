70b18bfdc19b8a3d1cfba0ee190098360ffce9eb
danieloneilldazn <daniel.oneill@dazn.com>
2021-03-11 18:44:30 +0100

    support for remove expense

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,6 +1,14 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import {addExpense, editExpense, removeExpense, startAddExpense, setExpenses, startSetExpenses} from '../../actions/expenses';
+import {
+    addExpense,
+    editExpense,
+    removeExpense,
+    startRemoveExpense,
+    startAddExpense,
+    setExpenses,
+    startSetExpenses
+} from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
 import database from '../../firebase/firebase';
 
@@ -21,6 +29,25 @@ test('should create remove expense object', () => {
         id: '123abc'
     })
 })
+
+test('should remove expense from firebase', (done) => {
+    const store = createMockStore({});
+    const id = expenses[0].id;
+    store.dispatch(startRemoveExpense({ id })).then(() => {
+        const actions = store.getActions();
+        expect(actions[0]).toEqual({
+            type: 'REMOVE_EXPENSE',
+            id
+        });
+
+        return database.ref(`expenses/${id}`).once('value');
+    }).then((snapshot) => {
+        expect(
+            snapshot.val()).toBeFalsy();
+        done();
+    })
+})
+
 
 test('should create edit expense object', () => {
     const action = editExpense('123abc', {note: '123'});
